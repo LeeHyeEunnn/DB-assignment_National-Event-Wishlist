f777b1929396ead4d5c2360e9d3dc0a0aad2736b
LeeHyeEunnn <gpdms6565@inu.ac.kr>
2025-11-27 20:10:24 +0900

    Show basic event list page using SQLite

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
+
+DB_NAME = "event_wishlist.db"
+
+
+def get_connection():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row  # 딕셔너리처럼 쓰려고
+    return conn
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/events")
+def events():
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT event_id, event_name, start_date, end_date, region, place
+        FROM Event
+        ORDER BY start_date
+        LIMIT 50
+    """)
+    rows = cur.fetchall()
+
+    conn.close()
+    return render_template("events.html", events=rows)
 
 
 if __name__ == "__main__":
